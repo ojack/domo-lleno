595343491686287c9a64d0afe9510fdf7f8e0272
OLIJAC <ojackson@gmail.com>
2018-10-16 15:16:02 -0500

    artist window adapts to screen

--- src/components/ArtistPopup.js
+++ src/components/ArtistPopup.js
@@ -23,23 +23,24 @@ class ArtistPopup extends Component {
         var image = this.props.post.acf.image ? 'url('+ this.props.post.acf.image.sizes.large +')' : '#222'
         return (
             <div className="fixed w-full h-full pin-t pin-l flex items-center justify-center flex-col" style={{background: 'rgba(0, 0, 0, 0.6)'}}>
-              <div className="w-full max-w-3xl flex flex-col" style={{background:'url(/static/images/festival-background0.png)', backgroundSize:'cover',   maxHeight: "65%"}}>
+              <div className="w-full max-w-3xl flex overflow-auto md:overflow-visible flex-col artist-popup" style={{background:'url(/static/images/festival-background0.png)', backgroundSize:'cover'}}>
+              <Link to='/artistas' className="absolute -ml-6 mt-6 self-end float-right cursor-pointer hover:opacity-50"><img className="self-end float-right cursor-pointer hover:opacity-50" src={Close} width="40px" height="40px"/></Link>
 
                 <div className="w-full flex flex-col md:flex-row text-left">
 
-                  <div className="flex-1 -ml-16 -mt-10 -mb-10" style={{
+                  <div className="flex-1 m-4 md:-ml-16 md:-mt-10 -mb-10" style={{
                     background: image,
                     backgroundSize:'contain',
                     backgroundRepeat: 'no-repeat',
                     backgroundPosition: 'center',
-                    maxWidth: '600px'
+                    maxWidth: '600px',
+                    minHeight: '280px'
 
                   }}></div>
-                  <div className="p-10 flex-1 leading-loose" style={{
+                  <div className="p-4 sm:p-10 flex-1 leading-loose" style={{
                     // maxHeight: "60%",
                     // overflowY: "auto"
                   }}>
-                    <Link to='/artistas'><img className="self-end float-right cursor-pointer hover:opacity-50" src={Close} width="40px" height="40px"/></Link>
                     <h1 className='font-menu uppercase text-3xl mb-6 mt-20' dangerouslySetInnerHTML={{__html: this.props.post.title.rendered}}></h1>
                     <div className=' mb-6 max-w-md mr-6' style={{ overflowY: "auto", maxHeight: '70%'}} dangerouslySetInnerHTML={{__html: this.props.post.content.rendered}}></div>
                   </div>
